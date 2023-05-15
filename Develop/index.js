f7f149ea4fb301a66a61d5ed393546b063b0a5dd
Mona Lisa <Sissy1310@aol.com>
2023-05-14 21:36:59 -0500

    added questions for index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
-
+const inquirer = inquire('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('.utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of this project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Why did you build this project?",
+        name: "why"
+    },
+    {
+        type: "input",
+        message: "What problem does it solve?",
+        name: "problem"
+    },
+    {
+        type: "input",
+        message: "What did you learn?",
+        name: "learn"
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
