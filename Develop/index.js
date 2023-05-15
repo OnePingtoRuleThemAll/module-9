20897c33468b55b3c079232b4d2f685d3bd2c544
Mona Lisa <Sissy1310@aol.com>
2023-05-14 21:57:09 -0500

    updated index.js with more questions

--- Develop/index.js
+++ Develop/index.js
@@ -25,12 +25,55 @@ const questions = [
         name: "learn"
     },
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "input",
+        message: "How can a user install this application?",
+        name: "install"
     },
-
-];
+    {
+        type: "input",
+        message: "Add a screenshot using ![alt text](.png)", //place screenshot info in here
+        name: "screenshot"
+    },
+    {
+        type: "input",
+        message: "What are the key features of the application?",
+        name: "features"
+    },
+    {
+        type: "input",
+        message: "List all collaborators and links to their GitHub Profile",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "Were there any third-party assets or tutorials used that require attribution?",
+        name: "thirdparty"
+    },
+    {
+        type: "input",
+        message: "How can a user test the application?",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "Add your GitHub so users can ask questions",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "How can other developers contribute?",
+        name: "contribute"
+    },
+].then((data)) => (
+    console.log(data);
+    fs.writeFile('README.md', generateMarkdown(data)),
+    error =>{
+    if (error) {
+            console.log('Please input all the data')
+        }
+        console.log('Your README was generated')
+    }
+);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
